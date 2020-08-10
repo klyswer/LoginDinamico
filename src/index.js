0b73246a3ac90273d27fe15153951acc89828e3b
klyswer <klyswer@gmail.com>
2020-08-10 00:36:12 -0400

    Probando el dispatch

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from './componentes/App'
 import './styles/styles.scss'
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import {getAllSpecialities, getAllCurses} from './redux/actionCreators'
+
+store.dispatch(getAllSpecialities())
+store.dispatch(getAllCurses())
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
   ,
   document.getElementById('root')
